723e61742a61a92acaaad01dbaad3120553a5ba8
Jean <jeanncavalcante@gmail.com>
2022-08-07 23:16:35 -0300

    register

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,6 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
@@ -8,6 +11,40 @@ module.exports = class AuthController {
   }
 
   static async registerPost(req, res) {
-    res.render("auth/register");
+    const { name, email, password, confirmpassword } = req.body;
+
+    if (password !== confirmpassword) {
+      req.flash("message", "As senhas não conferem");
+      res.render("auth/register");
+      return;
+    }
+
+    // verificar se o email já existe
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+
+    if (checkIfUserExists) {
+      req.flash("message", "Este email já está cadastrado");
+      res.render("auth/register");
+      return;
+    }
+
+    // criptografar a senha
+    const hashPassword = bcrypt.hashSync(password, 10);
+    const user = { name, email, password: hashPassword };
+    try {
+      const createUser = await User.create(user);
+
+      // initial session
+      req.session.userid = createUser.id;
+      req.flash("message", "Cadastro realizado com sucesso");
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (err) {
+      req.flash("message", "Erro ao cadastrar usuário");
+      res.render("auth/register");
+      console.log(err);
+      return;
+    }
   }
 };
